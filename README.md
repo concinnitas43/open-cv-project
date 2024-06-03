83a323195b9eab88be4b15fcf503890be4c86c84
concinnitas43 <concinnitas43@gmail.com>
2024-06-03 09:15:38 +0900

    image collected

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 키미노 나마에와
 asdfasdf
 asdfjklasdfjklasdfjkl
+
+snack 1 : 양파링
+snack 2 : 꿀꽈배기
+snack 3 :도도한 나쵸
+snack 3 :도도한 나쵸
+snack 3 :도도한 나쵸
--- snack_data_maker.py
+++ snack_data_maker.py
@@ -1,24 +1,37 @@
 import cv2
 
-is_there_snack = True
+is_there_snack = False
 attempt = 1
+
+snack_no = 2
 
 cap = cv2.VideoCapture(0)
 if cap.isOpened():
     frame_no = 0
     while True:
         ret, frame = cap.read()
+
+        frame = cv2.flip(frame, 1)
+
+        if cv2.waitKey(1) == 32:
+            is_there_snack = not is_there_snack
+            attempt += 1
+
+        if cv2.waitKey(1) == 113:
+            break
+
         if ret:
+            frame_no += 1
             if is_there_snack:
-                frame_no += 1
-                cv2.imshow('img', frame)
-                save_name = './snack_data/snack_yes/'+'img_'+str(is_there_snack)+'_'+str(attempt)+'_'+str(frame_no)+'.jpg'
+                save_name = f'./snack_data/snack{snack_no}/'+'img_'+str(is_there_snack)+'_'+str(attempt)+'_'+str(frame_no)+'.jpg'
                 cv2.imwrite(save_name, frame)
+                frame = cv2.rectangle(frame, (0, 0), (50, 50), (0, 255, 0), 2)
+                cv2.imshow('img', frame)
             else:
-                frame_no += 1
-                cv2.imshow('img', frame)
-                save_name = './snack_data/snack_no/'+'img_'+str(is_there_snack)+'_'+str(attempt)+'_'+str(frame_no)+'.jpg'
+                save_name = './snack_data/snack0/'+'img_'+str(is_there_snack)+'_'+str(attempt)+'_'+str(frame_no)+'.jpg'
                 cv2.imwrite(save_name, frame)
+                frame = cv2.rectangle(frame, (0, 0), (50, 50), (0, 0, 255), 2)
+                cv2.imshow('img', frame)
         else:
             print('no frame')
 else:
