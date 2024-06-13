91e30aa2f7dd4d5b4057e44b8f92a8460c29e74c
anfchd24 <anfchd24@naver.com>
2024-06-13 23:42:34 +0900

    ㅁㄴㅇㄹ

--- image_id.py
+++ image_id.py
@@ -1,23 +1,41 @@
-import dearpygui.dearpygui as dpg
-dpg.create_context()
+"""
+main.py를 보면 이미지를 저장해주는 함수가 있음에도 별도로 image_id.py를 만들었다.
+그 이유는 sender의 texture_tag를 print해보면 main의 경우 str: selcted or unselcted인데 
+fuctions에서는 그 값이 int와 불규칙적 숫자로 저장된다.
+아마 dearpygui라이브러리에서 int id로 tag를 관리하는 것 같은데, 그것은 서칭해도 못찾았다.
+그래서 id(int)를 찾아주는 과정이 필요해 별도의 iamge_id.py를 만든것이다.
+또 그 숫자가 내 컴퓨터 환경에 종속되는지조차 알 수 없어, 새로운 dpg.create_context()을 통해 객채를 만들어주고
+그것의 id를 저장해주었다
+"""
 
+
+import dearpygui.dearpygui as dpg
+dpg.create_context() # context 공간 생성
+
+#초기값 설정
 unselected_config=0
 selected_config=0
+
+"""
+텍스쳐 공간 맏들기
+
+이미지 불러오고 static(변하지않는 texture(보통 이미지 씀)) value 를 할당
+"""
 with dpg.texture_registry():
-    width, height, channels, data = dpg.load_image("./resources/circle_button_unselected.png")
-    unselected = dpg.add_static_texture(width=width, height=height, default_value=data, tag="unselected")
+    width, height, channels, data = dpg.load_image("./resources/circle_button_unselected.png") # 이미지 로드
+    unselected = dpg.add_static_texture(width=width, height=height, default_value=data, tag="unselected") # 이미지 객체로 할당
 
 with dpg.texture_registry():
-    width, height, channels, data = dpg.load_image("./resources/circle_button_selected.png")
-    selected = dpg.add_static_texture(width=width, height=height, default_value=data, tag="selected")
-
+    width, height, channels, data = dpg.load_image("./resources/circle_button_selected.png")# 이미지 로드
+    selected = dpg.add_static_texture(width=width, height=height, default_value=data, tag="selected") # 이미지 객체로 할당
+"""
+winodw 생성(딱히 파라미터값 할당 안해줘서 안보임)
+imagee_button add한 다음에 get_item_configuration 해서 int값 얻는다
+"""
 with dpg.window():
-    # Add buttons to check the assigned integer values
-    dpg.add_image_button(texture_tag=unselected, show=False, tag='UNSELECTED')
-    
-    dpg.add_image_button(texture_tag=selected, show=False, tag='SELECTED')
+    dpg.add_image_button(texture_tag=unselected, show=False, tag='UNSELECTED') # 버튼에 이미지 할당
+    dpg.add_image_button(texture_tag=selected, show=False, tag='SELECTED')  # 버튼에 이미지 할당
 
-    # Retrieve the configuration to check the texture tags
-    unselected_config = dpg.get_item_configuration('UNSELECTED')['texture_tag']
-    selected_config = dpg.get_item_configuration('SELECTED')['texture_tag']
-dpg.destroy_context()
+    unselected_config = dpg.get_item_configuration('UNSELECTED')['texture_tag'] # 버튼에서 이미지 ID(추정) int값 얻기
+    selected_config = dpg.get_item_configuration('SELECTED')['texture_tag'] # 버튼에서 이미지 ID(추정) int값 얻기
+dpg.destroy_context() # 초기화
--- main.py
+++ main.py
@@ -1,36 +1,57 @@
 from informations import *
 #from buttons_info import home_buttons, settings_buttons, names
-dpg.create_context()
+"""
+메인 파일이다.
+"""
+dpg.create_context() # dpg의 context들의 space를 create
+"""
+텍스텨 저장소
+selcted / unselcted 이미지를 저장한다
+"""
+with dpg.texture_registry(): # 저장소에서
+    width, height, channels, data = dpg.load_image("./resources/circle_button_unselected.png") #이미지 불러오고
+    unselected = dpg.add_static_texture(width=width, height=height, default_value=data, tag="unselected") #변수에 저장
 
-with dpg.texture_registry():
-    width, height, channels, data = dpg.load_image("./resources/circle_button_unselected.png")
-    unselected = dpg.add_static_texture(width=width, height=height, default_value=data, tag="unselected")
-
-with dpg.texture_registry():
+with dpg.texture_registry(): #아래와 동일하다
     width, height, channels, data = dpg.load_image("./resources/circle_button_selected.png")
     selected = dpg.add_static_texture(width=width, height=height, default_value=data, tag="selected")
+"""
+폰트 저장소
+폰트를 불러오고 사이즈별 폰트를 저장소에 저장해준다
+***dpg상에서 font의 size를 조절하는 함수가 없다. font자체에 사이즈가 포함되어 있는 포멧이다.***
+"""
+def font_size(n): # 폰트 저장해주는 함수
+    return dpg.add_font("./resources/Roboto-Light.ttf", n) # 폰트 추가하기
 
-def font_size(n: int):
-    return dpg.add_font("./resources/Roboto-Light.ttf", n)
-
-with dpg.font_registry():
+with dpg.font_registry(): # 폰트 저장소에서 사이즈별 저장 (5개)
     font10=font_size(10)
     font20=font_size(20)
     font30=font_size(30)
     font40=font_size(40)
     font50=font_size(50)
-
+"""
+변수 저장소
+tts.py에서 쓰일 변수들로
+이것에 따라 volume조절이 가능하고, TTS로 들려줄 정보의 종류를 고를 수 있다.
+"""
 with dpg.value_registry():
-    dpg.add_bool_value(default_value=False, tag='ProductName')
-    dpg.add_bool_value(default_value=False, tag='ProductPrice')
+    dpg.add_bool_value(default_value=False, tag='ProductName') # 테그(ID)가 ProductName인 bool타입 value 저장
+    dpg.add_bool_value(default_value=False, tag='ProductPrice') 
     dpg.add_bool_value(default_value=False, tag='EventInfo')
     dpg.add_bool_value(default_value=False, tag='ShortInfo')
     dpg.add_bool_value(default_value=False, tag='SpecificInfo')
-    dpg.add_int_value(default_value=30, tag='Volumn')
+    dpg.add_int_value(default_value=30, tag='Volume') # int타입 value 저장
 
+"""
+Button 객체 Class이다.
+라벨, 콜벡함수, 위치, width, height, tag 를 init으로 갖는다
 
+create 함수가 있는데 circle(selcted, unselcted)라면 
+    add_image_button으로 이미지 형식 저장 (dpg에는 버튼이 직사각형뿐이다)
+    아니라면 add_button으로 저장한다.
+"""
 class MyButton:
-    def __init__(self, label, click_callback, pos = None, width= None, height=None, unslec_num=None, selc_num=None):
+    def __init__(self, label, click_callback, pos = None, width= None, height=None):
         self.label = label
         self.click_callback = click_callback
         self.button_id = None
@@ -38,8 +59,6 @@ class MyButton:
         self.width = width
         self.height = height
         self.tag=label
-        self.unselc=unslec_num
-        self.selc=selc_num
 
     def create(self, circle=False, image=None):
         if circle:
@@ -49,38 +68,50 @@ class MyButton:
         else:
             self.button_id = dpg.add_button(label=self.label, callback=self.click_callback,\
                                                 pos = self.pos, width=self.width, height=self.height, tag=self.tag)
-
+"""
+button을 MyButton클래스로 만들어주는 과정.
+"""
 for buttonset in [home_buttons, settings_buttons]:
     for button_name in buttonset.keys():
         buttonset[button_name] = MyButton(label=f"{button_name}", click_callback=buttons__fucs[button_name], pos=buttonset[button_name][0],\
                                             width = buttonset[button_name][1][0], height = buttonset[button_name][1][1])
-with dpg.window(label="Setting Window", show=False, width=WIDTH, height=HEIGHT, pos=(0, 0), no_close=True, tag='setting_window'):
-    dpg.bind_font(font20)
-    for button in home_buttons.values():
+"""
+window를 생성하고 거기서 버튼을 넣어주고 폰트를 조절해준다
+"""
+with dpg.window(label="Setting Window", show=False, width=WIDTH, height=HEIGHT, pos=(0, 0), no_close=True, tag='setting_window'): # window 생성
+    dpg.bind_font(font20) # 전체 폰트 할당
+    for button in home_buttons.values(): # 버튼 만들기
         button.create()
-        dpg.bind_item_font(button.tag, font30)
-with dpg.window(label="Detailed Settings Window", show=False, width=WIDTH, height=HEIGHT, pos=(0, 0), no_close=True,tag='detailed_settings_window'):
-    dpg.bind_font(font20)
-    for button in settings_buttons.values():
+        dpg.bind_item_font(button.tag, font30) # 각 버튼의 폰트 할당
+with dpg.window(label="Detailed Settings Window", show=False, width=WIDTH, height=HEIGHT, pos=(0, 0), no_close=True,tag='detailed_settings_window'): # window 생성
+    dpg.bind_font(font20) # 전체 폰트 할당
+    for button in settings_buttons.values(): # 버튼 만들기
             if button.label in info_buttons:
-                button.create(circle=True, image=unselected)#, unselc_num=unselected_config, selc_num=selected_config)
+                if button.label == 'ProductName':
+                    button.create(circle=True, image=selected) # productname은 초기갑이 selcted
+                else:
+                    button.create(circle=True, image=unselected) # image 초기에는 uselcted
             else:
                 button.create()
-                dpg.bind_item_font(button.tag, font20)
-    s1 = dpg.add_text("Volumn", pos=(9/30*WIDTH-125,1/2*HEIGHT-150))
-    dpg.bind_item_font(s1, font40)
-    s2 = dpg.add_text("InforState", pos=(11/15*WIDTH-125,1/2*HEIGHT-150))
-    dpg.bind_item_font(s2, font40)
+                dpg.bind_item_font(button.tag, font20) # 버튼 폰트 설정
+    s1 = dpg.add_text("Volume", pos=(9/30*WIDTH-125,1/2*HEIGHT-150)) # 텍스트 Volume 넣기
+    dpg.bind_item_font(s1, font40) # Volum 폰트 설정
+    s2 = dpg.add_text("InforState", pos=(11/15*WIDTH-125,1/2*HEIGHT-150)) # inforstate 넣기 텍스트
+    dpg.bind_item_font(s2, font40) # inforstate 텍스트 폰트 설정
     for b, position in info_pos.items():
-        bt = dpg.add_text(b, pos=(position[0][0]+30, position[0][1]-5))
+        bt = dpg.add_text(b, pos=(position[0][0]+30, position[0][1]-5)) # inforstate 그룹의 애들 텍스트 설정 (productname, price 등)
+    # 슬라이드 만들기(volume 거)
     dpg.add_slider_int(pos=(1/5*WIDTH-60,1/2*HEIGHT),\
                        width=400, height=50,\
-                    track_offset=0.3, min_value=0, default_value=30, max_value=100, callback=Volumn_slider)
+                    track_offset=0.3, min_value=0, default_value=30, max_value=100, callback=Volume_slider)
 
-
+# viewport 만들어주기 (window는 화면 속 객체고 viewport가 전체이미지임)
 dpg.create_viewport(title='What is Your Name', width=WIDTH, height=HEIGHT+89)
+# 초기설정
 dpg.setup_dearpygui()
-
+#비디오 실행
 Video()
+#이후에 뷰포트 보여주기
 dpg.show_viewport()
+#dpg가 동적이게(start해주기)
 dpg.start_dearpygui()
